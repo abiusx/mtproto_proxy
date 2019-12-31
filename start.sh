4907bf5c29ddb48df8282d743b0417dd66187413
AbiusX <git@abiusx.com>
2019-12-31 16:42:10 +0300

    update to a more minimalistic verison

--- start.sh
+++ start.sh
@@ -7,22 +7,8 @@ THIS=$0
 
 usage() {
     echo "Usage:"
-    echo "To run with settings from config/prod-sys.config:"
-    echo "${THIS}"
-    echo "To start in single-port mode configured from command-line:"
-    echo "${THIS} -p <port> -s <secret> -t <ad tag>"
-    echo "To only allow connections with randomized protocol (dd-secrets):"
-    echo "${THIS} -a dd"
-    echo "Parameters:"
-    echo "-p <port>: port to listen on. 1-65535"
-    echo "-s <secret>: proxy secret. 32 hex characters 0-9 a-f"
-    echo "-t <ad tag>: promo tag that you get from @MTProxybot. 32 hex characters"
-    echo "-a dd: only allow 'secure' connections (with dd-secret) / fake-tls connections (base64 secrets)"
-    echo "-a tls: only allow 'fake-tls' connections (base64 secrets)"
-    echo "It's ok to provide both '-a dd -a tls'."
     echo "port, secret, tag and allowed protocols can also be configured via environment variables:"
-    echo "MTP_PORT, MTP_SECRET, MTP_TAG, MTP_DD_ONLY, MTP_TLS_ONLY"
-    echo "If both command line and environment are set, command line have higher priority."
+    echo "SECRET, TAG, MTP_DD_ONLY, MTP_TLS_ONLY"
 }
 
 error() {
@@ -30,46 +16,10 @@ error() {
     usage
     exit 1
 }
+PORT=${PORT:-"4444"}
 
 # check environment variables
-PORT=${MTP_PORT:-""}
-SECRET=${MTP_SECRET:-""}
-TAG=${MTP_TAG:-""}
-DD_ONLY=${MTP_DD_ONLY:-""}
-TLS_ONLY=${MTP_TLS_ONLY:-""}
-
-# check command line options
-while getopts "p:s:t:a:dh" o; do
-    case "${o}" in
-        p)
-            PORT=${OPTARG}
-            ;;
-        s)
-            SECRET=${OPTARG}
-            ;;
-        t)
-            TAG=${OPTARG}
-            ;;
-        a)
-            if [ "${OPTARG}" = "dd" ]; then
-                DD_ONLY="y"
-            elif [ "${OPTARG}" = "tls" ]; then
-                TLS_ONLY="y"
-            else
-                error "Invalid -a value: '${OPTARG}'"
-            fi
-            ;;
-        d)
-            echo "Warning: -d is deprecated! use '-a dd' instead"
-            DD_ONLY="y"
-            ;;
-        h)
-            usage
-            exit 0
-    esac
-done
-
-PROTO_ARG=""
+PROTO_ARG="-mtproto_proxy allowed_protocols [mtp_fake_tls,mtp_secure]" # default
 
 if [ -n "${DD_ONLY}" -a -n "${TLS_ONLY}" ]; then
     PROTO_ARG='-mtproto_proxy allowed_protocols [mtp_fake_tls,mtp_secure]'
